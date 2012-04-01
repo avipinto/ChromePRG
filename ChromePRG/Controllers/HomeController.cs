665d0d96624cd5977053433e28d9d3bf1ec594d0
avipinto <pinto.avi@gmail.com>
2012-04-01 14:56:15 +0300

    code

---
+++ ChromePRG/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ChromePRG.Controllers
+{
+	public class HomeController : Controller
+	{
+		public ActionResult Index()
+		{
+			return View();
+		}
+
+		public ActionResult NewSomething()
+		{
+			return View();
+		}
+
+		[HttpPost]
+		public ActionResult NewSomething(string someData)
+		{
+			return RedirectToAction("index");
+		}
+	}
+}
---
+++ ChromePRG/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ChromePRG.MvcApplication" Language="C#" %>
